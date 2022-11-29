63bda16388e5bc98b41d7cef8a5e81c706b1bcc3
Brandon <brandonkly@ucla.edu>
2022-11-29 13:29:25 -0800

    added question 1 and prompt for user

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer')
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        message: "What is the name of your project?",
+        name: "Name"
+
+    }
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+        .then(response => {
+            console.log(response)
+        })
+
+}
 
 // Function call to initialize app
 init();
