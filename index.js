df64e5c446ffe29d3938ad3d44dfd5b990c6fd6b
Brandon <brandonkly@ucla.edu>
2022-11-29 14:08:03 -0800

    can now write file based on user input

--- index.js
+++ index.js
@@ -1,26 +1,35 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer')
 const generateMarkdown = require('./utils/generateMarkdown')
+const fs = require('fs')
 // TODO: Create an array of questions for user input
 const questions = [
     {
         type: "input",
+        message: "What do you want your README file to be named?",
+        name: "fileName",
+    },
+    {
+        type: "input",
         message: "What is the title of your project?",
         name: "title",
-    }
+    },
 
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFileSync(fileName, data);
+}
 
 // TODO: Create a function to initialize app
 function init() {
     inquirer.prompt(questions)
         .then(response => {
             const md = generateMarkdown(response)
-            console.log(md)
-            console.log(response)
+            // console.log(md)
+            // writeToFile('REAME.md', md)
+            writeToFile(`${response.fileName}.md`, md)
         })
 
 }
