d23249c3b75e1cba012f5ecb3411e09b243c2cd0
Brandon <brandonkly@ucla.edu>
2022-12-06 14:35:06 -0800

    edited comments

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-// TODO: Include packages needed for this application
+
 const inquirer = require('inquirer')
 const generateMarkdown = require('./utils/generateMarkdown')
 const fs = require('fs')
-// TODO: Create an array of questions for user input
+
 const questions = [
     {
         type: "input",
@@ -63,12 +63,12 @@ const questions = [
     },
 ];
 
-// TODO: Create a function to write README file
+
 function writeToFile(fileName, data) {
     fs.writeFileSync(fileName, data);
 }
 
-// TODO: Create a function to initialize app
+
 function init() {
     inquirer.prompt(questions)
         .then(response => {
